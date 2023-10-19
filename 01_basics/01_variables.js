cdbff20617a201168bd1d2b13f0b5ab44bfae73d
Akshat Nigam <119806436+Akshat-nigam12@users.noreply.github.com>
2023-10-19 08:47:24 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "akshatnigam2020@gmail.com"
+var accountPassword = "12345"
+accountCity = "kanpur"
+
+// accountId  = 3 // not allowed
+
+accountEmail = "akshtgmail.com"
+accountPassword ="2341"
+accountCity = "Pune"
+
+console.table([accountId , accountEmail , accountPassword ,accountCity])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/ 
+
