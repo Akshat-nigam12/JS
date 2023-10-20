6cc7c1242ca17063d1edb9a3b8a76058aa80365e
Akshat Nigam <119806436+Akshat-nigam12@users.noreply.github.com>
2023-10-20 11:08:36 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,19 @@
+let newdate = new Date()
+// console.log(newdate.toString());
+// console.log(newdate.toDateString());
+// console.log(newdate.toISOString());
+// console.log(newdate.toLocaleString());
+// console.log(newdate.toLocaleDateString())
+
+
+let myCreateDate = new Date("1-14-2023")
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date();
+console.log(newDate.getDate());
