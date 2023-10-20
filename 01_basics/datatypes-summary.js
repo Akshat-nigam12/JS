22ede17c8c719527e4ca1159e4c5195ab6d17fc6
Akshat Nigam <119806436+Akshat-nigam12@users.noreply.github.com>
2023-10-20 05:41:23 +0000

    datatype summery

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+const id1 = Symbol('11')
+const id2 = Symbol('11')
+// console.table([id1,id2]);
+// console.log(id1===id2)
+
+const bigNumber = 32423415323332343434n  //n change it in bigint
+
+//Reference (Non primitive)
+
+//Array , Objects, function
+
+const heros = ["shakti", "naagraj", "doga"];
+
+let myobj = {
+    name:"akshat",
+    age: 32,
+}
+
+console.log(typeof bigNumber)
