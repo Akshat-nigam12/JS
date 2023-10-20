a9b44a1592e825c8dba63139becedb90d83c3083
Akshat Nigam <119806436+Akshat-nigam12@users.noreply.github.com>
2023-10-20 09:52:39 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,28 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(typeof score);
+// console.log(typeof balance);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(4));
+
+// const numbe=33.2651
+// console.log(numbe.toPrecision(4));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.random() *100+1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
+
