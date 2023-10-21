2e684575f4d90a090cb67bb9eb26897451efee66
Akshat Nigam <119806436+Akshat-nigam12@users.noreply.github.com>
2023-10-21 09:49:59 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,46 @@
+// array
+
+
+const myarr = [1, "Akshat"  ,3,6]
+// const newArr = new Array(1,4,6)
+
+// console.log(myarr[1]);
+
+// Array methods
+
+ 
+// myarr.push(7)
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr);
+
+// myarr.unshift(12)  // It insert the element at the beginning of array
+// console.log(myarr);
+// myarr.shift() // it remove the first element or you can say it shift everything at left by one
+// console.log(myarr);
+
+
+// console.log(myarr.includes(9)); // gives true or false
+// console.log(myarr.indexOf(6));  // gives index of element
+
+// const newAr = myarr.join() // it convert the element of myarr in string and seperate the accordingly which you write in the join
+// console.log(newAr);
+// console.log(typeof newAr); // its type is string
+
+// console.log("16" + myarr);
+
+
+// slice  ,   splice      ***
+
+
+
+
+
+
+
+
+
+
+
+
+
