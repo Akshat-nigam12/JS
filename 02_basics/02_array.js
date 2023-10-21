cd7cfe79ccd1817e41f052f95cafdaa3c3a75abf
Akshat Nigam <119806436+Akshat-nigam12@users.noreply.github.com>
2023-10-21 10:37:34 +0000

    array part 2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,34 @@
+const marvel_heros = ["Caption America" ,"spiderman" , "IronMan"]
+const dc_heros = ["superman", "batman"]
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+
+// const all_hero = marvel_heros.concat(dc_heros)
+// console.log(all_hero);
+
+//spread
+// const all_new_hero = [...marvel_heros , ...dc_heros]
+// console.log(all_new_hero);
+
+
+const another_array = [1,2,3,[4,5,[6,7,[8,9]]]]
+const change_array = another_array.flat(Infinity)
+// console.log(change_array);
+
+// console.log(Array.from("534523452"));
+
+let score1= 29
+let score2= 34
+let score3= 43
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
+
